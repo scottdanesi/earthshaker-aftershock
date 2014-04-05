d1064fb648ebe0e969ecd5f2941ea7b112877b26
Scott Danesi <scott@scottdanesi.com>
2014-04-05 10:20:50 -0500

    Player Stats reorganization

--- player.py
+++ player.py
@@ -30,32 +30,49 @@ class Player(procgame.game.Player):
 	def __init__(self, name):
 			super(Player, self).__init__(name)
 
-			#create player stats
+			### Create Player Stats Array ############################
 			self.player_stats = {}
 
-			### General Stats ###
+			### General Stats ########################################
 			self.player_stats['ball_in_play']=False
 
-			### Bonus and Status ###
-			self.player_stats['status']=''
-			self.player_stats['bonus_x']=1
-
-			### Ball Saver ###
+			### Ball Saver ###########################################
 			self.player_stats['ballsave_active']=False
 			self.player_stats['ballsave_timer_active']=False
 
-			### Skillshot ###
-			self.player_stats['skillshot_active']=False
-			self.player_stats['skillshot_x']=1
+			### Bonus and Status #####################################
+			self.player_stats['status']=''
+			self.player_stats['bonus_x']=1
 
-			### Center Ramp Stats ###
+			### Center Ramp Stats ####################################
 			self.player_stats['miles']=0
 			self.player_stats['center_shots']=0
 
-			### Jackpot Stats ###
-			self.player_stats['jackpot_level']=1
+			### Drop Target Stats ####################################
+			self.player_stats['drop_banks_completed']=0
 
-			### Zone Status Stats ###
+			### Jackpot Stats ########################################
+			self.player_stats['jackpot_level']=1
+			self.player_stats['total_jackpots_collected']=0
+			self.player_stats['last_multiball_jackpots_collected']=0
+
+			### Multiball Stats ######################################
+			self.player_stats['lock1_lit']=False
+			self.player_stats['lock2_lit']=False
+			self.player_stats['lock3_lit']=False
+			self.player_stats['multiball_running']=False
+			self.player_stats['balls_locked']=0
+
+			### Right Ramp Stats #####################################
+			self.player_stats['fault_visits']=0
+			self.player_stats['million_lit']=False
+
+			### Skillshot ############################################
+			self.player_stats['skillshot_active']=False
+			self.player_stats['skillshot_x']=1
+
+			### Zone Status Stats ####################################
+			self.player_stats['active_zone_limit']=3
 			self.player_stats['zones_visited']=0
 			self.player_stats['zone1_status']=-1
 			self.player_stats['zone2_status']=-1
@@ -66,22 +83,3 @@ class Player(procgame.game.Player):
 			self.player_stats['zone7_status']=-1
 			self.player_stats['zone8_status']=-1
 			self.player_stats['zone9_status']=-1
-
-			### Multiball Stats ###
-			self.player_stats['lock1_lit']=False
-			self.player_stats['lock2_lit']=False
-			self.player_stats['lock3_lit']=False
-			self.player_stats['multiball_running']=False
-			self.player_stats['balls_locked']=0
-
-			self.player_stats['fault_visits']=0
-			self.player_stats['inlanes_made']=0
-			self.player_stats['multiball_ready']=False
-			self.player_stats['multiball_started']=False
-			self.player_stats['million_lit']=False
-			self.player_stats['ball_start_time']=0
-			self.player_stats['drop_banks_completed']=0
-			self.player_stats['super_skillshots']=0
-			self.player_stats['secret_skillshots']=0
-			self.player_stats['modes_running']=False
-			self.player_stats['spinner_level']=0
