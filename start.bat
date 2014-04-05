8e0fe441350fd67f9b5c562c416abb84d5259a03
Scott Danesi <scott@scottdanesi.com>
2014-04-05 11:25:53 -0500

    Startup Script Updates

--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
 cd C:\GoogleDrive\earthshaker-aftershock
-REM del *.pyc
 ping -n 6 127.0.0.1 > nul
-python earthshaker.py
-pause
+call startFAST.bat
--- startFAST.bat
+++ startFAST.bat
@@ -1,7 +1,19 @@
-cd C:\GoogleDrive\earthshaker-aftershock
+REM #########################################################
+REM ### Earthshaker Windows Launcher with CMD Logging
+REM ### Created By Scott Danesi
+REM #########################################################
+
+REM ### Variables [Change these for your project] ###########
+set project_name=Earthshaker
+set project_path=C:\GoogleDrive\earthshaker-aftershock
+set output_path=C:\GoogleDrive\earthshaker-aftershock\stdout
+set project_python_filename=earthshaker.py
+
+REM ### System Variables [Do Not Change These] ##############
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
 set mytime=%mytime: =% 
-set FileName=EarthshakerStdOut_%mydate%_%mytime%
-python earthshaker.py >> "stdout\%FileName%.txt"
-pause
+set filename=%project_name%_StdOut_%mydate%_%mytime%
+
+REM ### Start Game ##########################################
+python %project_path%\%project_python_filename% >> "%output_path%\%filename%.txt" 2>&1
