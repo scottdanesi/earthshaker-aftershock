c5a1a7e233d01b415123d4504c8e34fab73a4a4a
Scott Danesi <scott@scottdanesi.com>
2014-10-10 08:36:00 -0500

    Super Jets Enhancements

--- mode_1.py
+++ mode_1.py
@@ -15,11 +15,11 @@
 #################################################################################
 
 #################################################################################
-##    _____ ____  _____   ___   ____________ 
-##   / ___// __ \/  _/ | / / | / / ____/ __ \
-##   \__ \/ /_/ // //  |/ /  |/ / __/ / /_/ /
-##  ___/ / ____// // /|  / /|  / /___/ _, _/ 
-## /____/_/   /___/_/ |_/_/ |_/_____/_/ |_|  
+##    _____ __  ______  __________         ___________________
+##   / ___// / / / __ \/ ____/ __ \       / / ____/_  __/ ___/
+##   \__ \/ / / / /_/ / __/ / /_/ /  __  / / __/   / /  \__ \ 
+##  ___/ / /_/ / ____/ /___/ _, _/  / /_/ / /___  / /  ___/ / 
+## /____/\____/_/   /_____/_/ |_|   \____/_____/ /_/  /____/  
 ## 
 #################################################################################
 
@@ -34,14 +34,36 @@ class Mode1(game.Mode):
 			super(Mode1, self).__init__(game, priority)
 			
 	def mode_started(self):
-		self.game.utilities.set_player_stats('mode1_status',1)
-		self.game.utilities.score(25000)
-		self.game.shelter_mode.refreshPlayerInfo()
-		self.game.update_lamps()
-		self.game.modes.remove(self)
+		self.game.utilities.set_player_stats('mode1_status',0)
+		self.update_lamps()
 
 	def mode_stopped(self):
-		pass
+		self.game.utilities.set_player_stats('mode1_status',1)
+
+	def update_lamps(self):
+		print "Update Lamps: Mode 1 Jet Bumpers"
+		self.game.lamps.jetLeftLamp.schedule(schedule=0x000F000F, cycle_seconds=0, now=True)
+		self.game.lamps.jetRightLamp.schedule(schedule=0xF0F0F0F0, cycle_seconds=0, now=True)	
+		self.game.lamps.jetTopLamp.schedule(schedule=0x0F000F00, cycle_seconds=0, now=True)	
+		self.game.lamps.jetCenter.schedule(schedule=0x0F0F0F0F, cycle_seconds=0, now=True)	
+
+	def sw_jetLeft_active(self, sw):
+		self.game.sound.play('jet_super')
+		self.game.utilities.acFlashPulse(coilname='californiaFault_CenterRampFlashers3',pulsetime=60)
+		self.game.utilities.score(5000)
+		return procgame.game.SwitchStop
+
+	def sw_jetRight_active(self, sw):
+		self.game.sound.play('jet_super')
+		self.game.utilities.acFlashPulse(coilname='californiaFault_CenterRampFlashers3',pulsetime=60)
+		self.game.utilities.score(5000)
+		return procgame.game.SwitchStop
+
+	def sw_jetTop_active(self, sw):
+		self.game.sound.play('jet_super')
+		self.game.utilities.acFlashPulse(coilname='californiaFault_CenterRampFlashers3',pulsetime=60)
+		self.game.utilities.score(5000)
+		return procgame.game.SwitchStop
 		
 		
 
